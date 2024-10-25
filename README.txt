0f90028181e236ff6f1ce7329bf44e8b50d166bf
SaltyKeys <larsen.d.nathan@gmail.com>
2024-10-25 14:44:32 -0600

    first commit

---
+++ README.txt
@@ -0,0 +1,15 @@
+This is the initial prototype for water right transactions.
+
+For intial setup I followed this guide: https://code.visualstudio.com/docs/python/tutorial-django
+The python environment matters.
+
+
+Notes:
+tailwindcss?
+
+
+to add a new page, need to add to views, urls, and templates.
+
+
+views.py (that contains the functions that define pages in your web app)
+models.py (that contains classes defining your data objects)
---
+++ watermain/views.py
@@ -0,0 +1,20 @@
+import re
+from django.utils.timezone import datetime
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return HttpResponse("Hello, Django!")
+
+def hello_there(request, name):
+    print(request.build_absolute_uri()) #optional
+    return render(
+        request,
+        'watermain/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
+
